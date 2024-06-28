33cf095c28f6b85c3cb2fb0ba9d4c8282872a7f9
zachvalenta <zjv5002@gmail.com>
2024-06-28 14:31:52 -0400

    fix Makefile cmd repl by reinstalling bpython

--- repl.py
+++ repl.py
@@ -5,8 +5,8 @@ import re
 print("\nSOURCE MODULES: \n")
 for mod in dir(sys.modules[__name__]):
     if (
-        re.search("__\w", mod) is None and
-        re.search("^(help|sys|re)$", mod) is None
+        re.search(r"__\w", mod) is None and
+        re.search(r"^(help|sys|re)$", mod) is None
     ):
         print(mod)
 
